f6f6288f8f0d1e832acadbf975f1ce4467947b7f
Panuwat <panuwat_jarujareet@hotmail.com>
2021-05-28 13:10:54 +0700

    change icon

---
+++ Assets/Scripts/FirebaseInitialize.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Firebase;
+using Firebase.Analytics;
+using UnityEngine;
+
+public class FirebaseInitialize : MonoBehaviour
+{
+    public static bool IsReadyToUseFirebase;
+
+    private void Awake()
+    {
+        if(IsReadyToUseFirebase)
+        {
+            Destroy(this);
+        }
+    }
+
+    private void Start() {
+        // Initialize Firebase
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == DependencyStatus.Available) {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                // Crashlytics will use the DefaultInstance, as well;
+                // this ensures that Crashlytics is initialized.
+                FirebaseApp app = FirebaseApp.DefaultInstance;
+
+                Debug.Log("Firebase available");
+
+                // Set a flag here for indicating that your project is ready to use Firebase.
+                IsReadyToUseFirebase = true;
+                FirebaseAnalytics.LogEvent(FirebaseAnalytics.EventAppOpen);
+                Firebase.Messaging.FirebaseMessaging.TokenReceived += OnTokenReceived;
+                Firebase.Messaging.FirebaseMessaging.MessageReceived += OnMessageReceived;
+
+            }
+            else {
+                UnityEngine.Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+        DontDestroyOnLoad(this);
+    }
+    
+    
+    
+    private void OnTokenReceived(object sender, Firebase.Messaging.TokenReceivedEventArgs token)
+    {
+        UnityEngine.Debug.Log("Received Registration Token: " + token.Token);
+    }
+
+    private void OnMessageReceived(object sender, Firebase.Messaging.MessageReceivedEventArgs e)
+    {
+        UnityEngine.Debug.Log("Received a new message from: " + e.Message.From);
+    }
+}
